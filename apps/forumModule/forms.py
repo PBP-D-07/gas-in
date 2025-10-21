0461f17b4bb0d5ece7015886676695715bd0c899
“keiraislearning” <Keirana18@gmail.com>
2025-10-22 01:31:10 +0700

    feat: create forms for create post

--- apps/forumModule/forms.py
+++ apps/forumModule/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
 from .models import Post
+from django.utils.html import strip_tags
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        return strip_tags(name)
+
+    def clean_description(self):
+        description = self.cleaned_data["description"]
+        return strip_tags(description)
 
