8af901e1996cb8aa16b66a329caa30dbf403333e
“keiraislearning” <Keirana18@gmail.com>
2025-10-24 12:38:04 +0700

    feat: adding element to forumMain.html and post_detail.html

--- apps/forumModule/urls.py
+++ apps/forumModule/urls.py
@@ -1,27 +1,11 @@
-# from django.urls import path
-# from apps.forumModule.views import add_comment, get_comments, show_main, create_post, edit_post, delete_post, show_post, show_json, show_json_by_id, toggle_like
-
-
-# app_name = 'forumModule'
-
-# urlpatterns = [
-#     path('', show_main, name='show_main'),
-#     path('create-post/', create_post, name='create_post'),
-#     path('edit/<str:id>/', edit_post, name='edit_post'),
-#     path('delete/<str:id>/', delete_post, name='delete_post'),
-#     path('json/<str:post_id>/', show_json_by_id, name='show_json_by_id'),
-#     path('json/', show_json, name='show_json'),
-#     path('<str:post_id>/', show_post, name='show_post'),
-#     path('like/<uuid:post_id>/', toggle_like, name='toggle_like'),
-#     path("comment/<str:post_id>/", get_comments, name="get_comments"),
-#     path("comment/<str:post_id>/add/", add_comment, name="add_comment"),
-# ]
 from django.urls import path
 from apps.forumModule import views
+from django.contrib import admin 
 
 app_name = 'forumModule'
 
 urlpatterns = [
+    path('admin-django/', admin.site.urls),
     path('', views.show_main, name='show_main'),
     path('create/', views.create_post, name='create_post'),
     path('json/', views.show_json, name='show_json'),
