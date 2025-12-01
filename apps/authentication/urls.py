78dbecd660f15bd1762d581a61aa59eaac133ea3
anotherbondan <anandagautama3112@gmail.com>
2025-12-01 21:15:37 +0700

    feat: add url for method current user

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from apps.authentication.views import login, register
+from apps.authentication.views import login, register, current_user
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('current-user/', current_user, name='current_user'),
 ]
