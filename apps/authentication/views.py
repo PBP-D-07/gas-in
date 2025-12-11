9141816d2de6afc4f6ca314a42d4d568025ce9bb
satirahnurul <tirahnurul51@gmail.com>
2025-12-11 16:41:40 +0700

    Tambah logout di authentication

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.views.decorators.csrf import csrf_exempt
 from apps.main.models import User
 import json
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 @csrf_exempt
 def register(request):
@@ -65,4 +68,20 @@ def login(request):
         return JsonResponse({
             "status": False,
             "message": "Login failed, please check your username or password."
+        }, status=401)
+    
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+            "status": False,
+            "message": "Logout failed."
         }, status=401)
